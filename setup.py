92efa429592298cc83a978a7fa96ec2fdb8b98ff
diogenes42069 <105167885+diogenes42069@users.noreply.github.com>
2022-05-08 15:08:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,21 @@
 import os
+import pickle
+import settings
 
+from problem import *
+
+def initialize():
+    """
+    Returns a new set of multiplication problems if this is the first use. Otherwise, loads historical problems.
+    """
+    return generateProblems()
+    
+    # if os.path.exists(settings.savefile) == False:
+
+def generateProblems():
+    """Returns a list of all multiplication problems with factors from 1 to 10 (inclusive)"""
+    allProblems = []
+    for a in range(1,11):
+        for b in range(1,11):
+            allProblems.append(Problem(a,b))
+    return allProblems    
